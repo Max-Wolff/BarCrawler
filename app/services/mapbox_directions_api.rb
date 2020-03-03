3b64f0e52151027562e5f434563ed4462d11759c
Moritz Löchner <momo.loechner@gmail.com>
2020-03-03 18:17:00 +0100

    implemented an API service that can be called with an array of Bar instances to return the coordinates of the route in geojson format

---
+++ app/services/mapbox_directions_api.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'open-uri'
+
+def directions(bars)
+  bar_latlon = ''
+  bars.each do |bar|
+    bar_latlon += "#{bar.longitude},#{bar.latitude};"
+  end
+  url = "https://api.mapbox.com/directions/v5/mapbox/walking/#{bar_latlon[0...-1]}?geometries=geojson&access_token=#{ENV['MAPBOX_API_KEY']}"
+  serialized = open(url).read
+  result = JSON.parse(serialized)
+  coordinates = result['routes'].first['geometry']['coordinates']
+
+  return coordinates
+end
