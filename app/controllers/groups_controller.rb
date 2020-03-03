1abf74559592589a9d38dd03605997e619a6781d
Gerwin Fricke <gerwin.fricke@preplounge.com>
2020-03-03 13:11:34 +0100

    added route and styling

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,5 +6,6 @@ class GroupsController < ApplicationController
   def create
     @group = Group.create(token: params[:authenticity_token], name: params[:group][:name])
     @group.save
+    redirect_to unique_group_path(params[:authenticity_token])
   end
 end
