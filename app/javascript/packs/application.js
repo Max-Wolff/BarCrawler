3787759227ab40ec0690830cff8dd8c8c8013fa6
Gerwin Fricke <gerwin.fricke@preplounge.com>
2020-03-04 21:05:50 +0100

    loader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,6 @@ import { initAutocomplete } from '../plugins/init_autocomplete';
 import { showSliderValue } from '../components/range_slider';
 import { initMapbox } from '../plugins/init_mapbox';
 
-
 bannerCarousel();
 initAutocomplete();
 showSliderValue();
