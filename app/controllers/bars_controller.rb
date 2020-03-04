84094dd5dee77c68b821277ceb2760aa35ef3d36
Moritz Löchner <momo.loechner@gmail.com>
2020-03-04 13:07:23 +0100

    limeted the selection of bars to seven

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -4,13 +4,11 @@ class BarsController < ApplicationController
 
   def index
     # foursquare
-    hash_one = {name: "Ähm",rating: 2.7, address:"Neukölnstrasse 12, 1232 Berlin", price: 1, picture_url:"https://www.jaz-hotel.com/cache/images/wolframslider3_008a741a8a741a5d7a33e2-1-1.jpg", category:"Bar", open_until: "Open until 1:00 AM", open_today: false}
-    hash_two = {name: "Muted Horn",rating: 4.2, address:"Flughafenstr 49, 12053 Berlin", price: 2, picture_url:"https://www.jaz-hotel.com/cache/images/wolframslider3_008a741a8a741a5d7a33e2-1-1.jpg", category:"Bar", open_until: "Open until 12:00 PM", open_today: true}
-    bar1 = Bar.create(hash_one)
-    bar2 = Bar.create(hash_two)
+    bars = Bar.first(10)
     @group = Group.create
-    Stop.create(group: @group, bar: bar1)
-    Stop.create(group: @group, bar: bar2)
+    bars.each do |bar|
+      Stop.create(group: @group, bar: bar)
+    end
 
     redirect_to show_group_path(@group)
   end
