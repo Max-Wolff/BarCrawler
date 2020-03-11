99fb30cff29496620e62012eeb40608b40251ece
Max Wolff <maxwolff1103@gmail.com>
2020-03-11 16:50:52 +0100

    hi

--- app/javascript/components/typer.js
+++ app/javascript/components/typer.js
@@ -5,7 +5,7 @@ const typerJs = () => {
   const element = document.querySelector('.typer-js');
   if (element) {
       typer(element, 100).cursor({block: false, blink: 'hard', color: 'white'})
-      .line('Other Groups chose the following names:').pause(2000)
+      .line('Best Team Names so far:').pause(2000)
       .line('The Brew Crew 🍻').pause(1000).back('all').continue('Raging Alcoholics 🤫').pause(1000).back('all')
       .continue('Saturdays are for the Boys').pause(1000).back('all').continue('Two girls 10 cups ').pause(2000).continue('😑').pause(1000).back('all')
       .continue('Uncreative Drinkers 😉').pause(1000).back('all').continue('The drinkers & the lightweight').pause(1000).back('all').continue('We all know who this is 😁')
