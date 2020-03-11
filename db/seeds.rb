5dd87e423d5438207e08584f4f89ed6f25873670
Moritz Löchner <momo.loechner@gmail.com>
2020-03-11 16:51:22 +0100

    small adjustments at the wheel

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Clearing the database..."
+
+DrinkStop.destroy_all
 Stop.destroy_all
 Bar.destroy_all
 Group.destroy_all
 Drink.destroy_all
+
+puts "Adding 9 bars..."
 
 bar_1 = Bar.create(name: "Ä",rating: 4, address:"Flughafenstr 12, 1232 Berlin", price:3, photo_url:"https://www.jaz-hotel.com/cache/images/wolframslider3_008a741a8a741a5d7a33e2-1-1.jpg", category:"Bar", open_until: "Until 1:00 AM", open_today: false, latitude: 52.507859, longitude: 13.391951)
 bar_3 = Bar.create(name: "Muted Horn",rating: 4.2, address:"Flughafenstr 49, 12053 Berlin", price:2, photo_url:"https://www.jaz-hotel.com/cache/images/wolframslider3_008a741a8a741a5d7a33e2-1-1.jpg", category:"Nightclub", open_until: "Until 12:00 PM", open_today: true, latitude: 52.5079, longitude: 13.339951)
@@ -21,11 +26,10 @@ bar_8 = Bar.create(name: "Thelonious Bar",rating: 2, address:"Weserstrasse 202, 
 bar_9 = Bar.create(name: "Klunkerkranich",rating: 1.5, address:"Karl-Marx-Str 62, 12043 Berlin", price:2, photo_url:"https://www.jaz-hotel.com/cache/images/wolframslider3_008a741a8a741a5d7a33e2-1-1.jpg", category:"Gerwin's Mum", open_until: "Until 1:00 AM", open_today: true, latitude: 52.50, longitude: 13.39)
 bar_10 = Bar.create(name: "Schwelgerei",rating: 5, address:"Flughafenstr. 49, 12053 Berlin", price:2, photo_url:"https://www.jaz-hotel.com/cache/images/wolframslider3_008a741a8a741a5d7a33e2-1-1.jpg", category:"Bar", open_until: "Until 1:00 AM", open_today: true, latitude: 52.507859, longitude: 13.391951)
 
-group_1 = Group.create(name:"Guys just want to have fun",token:"")
-group_4 = Group.create(name:"The champions",token:"")
+puts "Adding 14 drinks..."
 
-Drink.create(name: 'Beer', picture_url: 'https://cdn.pixabay.com/photo/2018/08/21/21/49/beer-3622237_960_720.png')
-Drink.create(name: 'Tequila', picture_url: 'https://i.dlpng.com/static/png/5544536-tequila-png-abeoncliparts-cliparts-vectors-for-free-2019-tequila-shots-png-1200_1200_preview.png')
+Drink.create(name: 'Beer', picture_url: 'https://pngimage.net/wp-content/uploads/2018/05/beer-png-5.png')
+Drink.create(name: 'Tequila', picture_url: 'https://pngimage.net/wp-content/uploads/2018/06/shots-tequila-png-3.png')
 Drink.create(name: 'Berliner Luft', picture_url: 'https://www.konsum-leipzig.de/media/image/b9/7c/b2/DV019_4013228325202_VORvRXP9oGhXhcN0_600x600.png')
 Drink.create(name: 'Jägermeister', picture_url: 'https://www.konsum-leipzig.de/media/image/4b/4c/4a/DV019_4067700011015_VORxaFVIK0yWdPT2.png')
 Drink.create(name: 'Vodka', picture_url: 'https://www.konsum-leipzig.de/media/image/6e/f3/0a/DV019_7312040017683_VORBgtJOyFcjUX8v_600x600.png')
@@ -38,3 +42,5 @@ Drink.create(name: 'Moscow Mule', picture_url: 'https://www.proofcocktail.com/th
 Drink.create(name: 'Vodka Energy', picture_url: 'https://www.pngkey.com/png/full/50-507103_vodka-red-bull-bundle-vodka-red-bull-png.png')
 Drink.create(name: 'White Wine', picture_url: 'https://pngimage.net/wp-content/uploads/2018/06/white-wine-glass-png-5.png')
 Drink.create(name: 'Red Wine', picture_url: 'https://pngimage.net/wp-content/uploads/2018/06/glass-of-red-wine-png.png')
+
+puts "Done!"
