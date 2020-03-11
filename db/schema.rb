e3cb62c7dd6cee808c9a2042f6f17f35d523ed18
Moritz Löchner <momo.loechner@gmail.com>
2020-03-11 15:57:58 +0100

    added filter placeholder and apple hamburger toggle. MIGRATE @g-win

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_181049) do
+ActiveRecord::Schema.define(version: 2020_03_11_142327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_181049) do
     t.integer "distance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
   end
 
   create_table "members", force: :cascade do |t|
